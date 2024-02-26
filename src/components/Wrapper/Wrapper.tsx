bfd8b05b57d2354b213055c9166cb37e067983dc
ldemyanov <leonidemyanov@gmail.com>
2024-02-27 03:44:34 +0800

    feat: first deploy

--- src/components/Wrapper/Wrapper.tsx
+++
@@ -1,5 +0,0 @@
-import css from "./Wrapper.module.css";
-
-const Wrapper: React.FC<React.PropsWithChildren> = ({ children }) => <div className={css.wrapper}>{children}</div>;
-
-export default Wrapper;
--- src/constants.ts
+++
@@ -1,7 +0,0 @@
-export const LOAD_LIMIT = 52;
-export const VISIBLE_LIMIT = 50;
-export const EMPTY_BRAND = "Выберите бренд";
-
-export const PROP_BRAND = "brand";
-export const PROP_PRODUCT = "product";
-export const PROP_PRICE = "price";
